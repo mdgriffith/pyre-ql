3bb15c15825233fab0787bf2b7bf23b2191f3cc5
Matthew Griffith <mdg.griffith@gmail.com>
2024-07-19 12:55:46 -0400

    simpler call

--- src/generate/typescript.rs
+++ src/generate/typescript.rs
@@ -459,7 +459,7 @@ fn to_query_file(
         if written_field {
             result.push_str(", ");
         }
-        let sql = crate::generate::sql::to_string(context, query, &vec![field]);
+        let sql = sql::to_string(context, query, &table, field);
         result.push_str(&literal_quote(&sql));
         written_field = true;
     }
