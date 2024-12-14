a04f38c87efb84626070e519dc51b453c6329fc7
Matthew Griffith <mdg.griffith@gmail.com>
2024-12-14 10:52:04 -0500

    Add format on save for the vscode extension

---
+++ editor/pyre-lang/src/commands/format.ts
@@ -0,0 +1,31 @@
+import { spawn } from "child_process";
+
+const pyreCommand = "/Users/mattgriffith/projects/vendr/pyre-ql/target/debug/pyre" // "pyre"
+
+export async function format(input: string, filepath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const pyreProcess = spawn(pyreCommand, ["format", filepath]);
+    let stdout = "";
+    let stderr = "";
+
+    pyreProcess.stdout.on("data", (data) => {
+      stdout += data.toString();
+    });
+
+    pyreProcess.stderr.on("data", (data) => {
+      stderr += data.toString();
+    });
+
+    pyreProcess.on("close", (code) => {
+      if (code === 0) {
+        resolve(stdout);
+      } else {
+        reject(new Error(`Pyre format failed: ${stderr}`));
+      }
+    });
+
+    pyreProcess.stdin.write(input);
+    pyreProcess.stdin.end();
+  });
+}
+
--- editor/pyre-lang/src/extension.ts
+++ editor/pyre-lang/src/extension.ts
@@ -1,25 +1,34 @@
 import * as vscode from "vscode";
-import { spawn } from "child_process";
+import { format } from "./commands/format";
+import {checkErrors} from "./commands/errorCheck";
+
+const pyre = vscode.window.createOutputChannel("Pyre");
 
 export function activate(context: vscode.ExtensionContext) {
+
+
   // Register the formatting provider
-  const provider = vscode.languages.registerDocumentFormattingEditProvider(
+  const formattingProvider = vscode.languages.registerDocumentFormattingEditProvider(
     "pyre",
     {
       async provideDocumentFormattingEdits(
         document: vscode.TextDocument,
       ): Promise<vscode.TextEdit[]> {
         try {
+          pyre.appendLine("Formatting")
           const text = document.getText();
-          const formattedText = await formatPyre(text, document.fileName);
-
+          const formattedText = await format(text, document.fileName);
+         
           const fullRange = new vscode.Range(
             document.positionAt(0),
             document.positionAt(document.getText().length),
           );
+          pyre.appendLine("Replacing")
+
 
           return [vscode.TextEdit.replace(fullRange, formattedText)];
         } catch (error) {
+          pyre.appendLine(`Error formatting Pyre file: ${error}`)
           vscode.window.showErrorMessage(
             `Error formatting Pyre file: ${error}`,
           );
@@ -28,43 +37,55 @@ export function activate(context: vscode.ExtensionContext) {
       },
     },
   );
+  context.subscriptions.push(formattingProvider);
 
-  // Register the command
-  let disposable = vscode.commands.registerCommand("pyre.format", () => {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      vscode.commands.executeCommand("editor.action.formatDocument");
+
+
+  // Dignostics
+  const diagnostics = vscode.languages.createDiagnosticCollection('pyre');
+  context.subscriptions.push(diagnostics);
+
+  // Listen for document save events and trigger diagnostics
+  vscode.workspace.onDidSaveTextDocument((document) => {
+    if (document.languageId === 'pyre') {
+      checkErrors(document, diagnostics);
     }
   });
 
-  context.subscriptions.push(provider, disposable);
+
 }
 
-async function formatPyre(input: string, filepath: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const pyreProcess = spawn("pyre", ["format", filepath]);
-    let stdout = "";
-    let stderr = "";
 
-    pyreProcess.stdout.on("data", (data) => {
-      stdout += data.toString();
-    });
+// const pyreCommand = "/Users/mattgriffith/projects/vendr/pyre-ql/target/debug/pyre" // "pyre"
 
-    pyreProcess.stderr.on("data", (data) => {
-      stderr += data.toString();
-    });
+// async function formatPyre(input: string, filepath: string): Promise<string> {
+//   return new Promise((resolve, reject) => {
+//     const pyreProcess = spawn(pyreCommand, ["format", filepath]);
+//     let stdout = "";
+//     let stderr = "";
 
-    pyreProcess.on("close", (code) => {
-      if (code === 0) {
-        resolve(stdout);
-      } else {
-        reject(new Error(`Pyre format failed: ${stderr}`));
-      }
-    });
+//     pyreProcess.stdout.on("data", (data) => {
+//       stdout += data.toString();
+//     });
 
-    pyreProcess.stdin.write(input);
-    pyreProcess.stdin.end();
-  });
-}
+//     pyreProcess.stderr.on("data", (data) => {
+//       stderr += data.toString();
+//       pyre.appendLine(`Err: ${data.toString()}`)
+//     });
 
-export function deactivate() {}
+//     pyreProcess.on("close", (code) => {
+//       pyre.appendLine(`Close: ${code}`)
+//       pyre.appendLine(stdout)
+//       if (code === 0) {
+//         resolve(stdout);
+//       } else {
+//         reject(new Error(`Pyre format failed: ${stderr}`));
+//       }
+//     });
+
+//     pyreProcess.stdin.write(input);
+//     pyreProcess.stdin.end();
+//   });
+// }
+
+export function deactivate() { }
