219cd597bb74458d69384d2d62d181370e45eb56
Matthew Griffith <mdg.griffith@gmail.com>
2025-03-30 17:16:00 -0700

    Implement migrate::push

--- src/command/migrate.rs
+++ src/command/migrate.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 
 use super::shared::{check_namespace_requirements, parse_database_schemas, Options};
 use crate::ast;
+use crate::ast::diff;
 use crate::db;
 use crate::error;
 use crate::filesystem;
@@ -58,12 +59,121 @@ pub async fn migrate<'a>(
                             println!("Migration finished!");
                         }
                         Err(migration_error) => {
-                            println!("{}", migration_error.format_error());
+                            println!("{:?}", migration_error);
                         }
                     }
                 }
                 Err(err) => {
-                    println!("{}", err.format_error());
+                    println!("{:?}", err);
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+/**
+ * This is the new "dynamic" migration approach
+ *
+ *
+ *
+ */
+pub async fn push<'a>(
+    options: &'a Options<'a>,
+    database: &str,
+    auth: &Option<String>,
+    migration_dir: &str,
+    namespace: &Option<String>,
+) -> io::Result<()> {
+    check_namespace_requirements(&namespace, &options);
+    let namespace_migration_dir = match namespace {
+        Some(ns) => Path::new(migration_dir).join(ns),
+        None => Path::new(migration_dir).to_path_buf(),
+    };
+
+    // Get schema
+    let paths = filesystem::collect_filepaths(&options.in_dir)?;
+    let all_schemas = parse_database_schemas(&paths)?;
+
+    let real_namespace = match namespace {
+        Some(ns) => ns,
+        None => &ast::DEFAULT_SCHEMANAME.to_string(),
+    };
+
+    // Get exactly one schema based on namespace or default
+    let current_schema = all_schemas
+        .schemas
+        .iter()
+        .find(|schema| schema.namespace == *real_namespace)
+        .ok_or_else(|| {
+            eprintln!("Error: No schema found for namespace '{}'", real_namespace);
+            std::process::exit(1);
+        });
+    let current_schema = current_schema.unwrap(); // Safe to unwrap after error handling above
+
+    // Typecheck schemas
+
+    match typecheck::check_schema(&all_schemas) {
+        Err(error_list) => {
+            error::report_and_exit(error_list, &paths);
+        }
+        Ok(context) => {
+            let connection_result = db::connect(&database.to_string(), auth).await;
+            match connection_result {
+                Err(err) => {
+                    println!("{:?}", err);
+                }
+                Ok(conn) => {
+                    let introspection_result = crate::db::introspect::introspect(&conn).await;
+                    match introspection_result {
+                        Ok(introspection) => {
+                            if let Some(ref db_recorded_schema) = introspection.schema {
+                                let schema_diff =
+                                    diff::diff_schema(&current_schema, db_recorded_schema);
+
+                                // We diff the two schemas and report errors.
+
+                                let errors = diff::to_errors(schema_diff);
+                                if !errors.is_empty() {
+                                    error::report_and_exit(errors, &paths);
+                                }
+
+                                // If there are no errors, we can now generate sql.
+
+                                let db_diff = crate::db::diff::diff(
+                                    &context,
+                                    &current_schema,
+                                    &introspection,
+                                );
+
+                                // Generate sql
+                                let sql = crate::db::diff::to_sql::to_sql(&db_diff);
+
+                                let conn = conn.connect().unwrap();
+                                let tx = conn
+                                    .transaction_with_behavior(
+                                        libsql::TransactionBehavior::Immediate,
+                                    )
+                                    .await
+                                    .unwrap();
+
+                                tx.execute_batch(&sql).await.unwrap();
+
+                                tx.commit().await.unwrap();
+
+                                std::process::exit(1);
+                            } else {
+                                println!(
+                                    "No schema found in the databasefor namespace '{}'",
+                                    real_namespace
+                                );
+                                std::process::exit(1);
+                            }
+                        }
+                        Err(err) => {
+                            println!("{:?}", err);
+                        }
+                    }
                 }
             }
         }
