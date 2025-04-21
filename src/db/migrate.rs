6d4af8b93e7b15fc104e2c4aa52ca44e8a4c5880
Matthew Griffith <mdg.griffith@gmail.com>
2025-04-21 07:57:02 -0400

    SQL updates

--- src/db/migrate.rs
+++ src/db/migrate.rs
@@ -2,26 +2,31 @@ pub const MIGRATION_TABLE: &str = "_pyre_migrations";
 
 pub const SCHEMA_TABLE: &str = "_pyre_schema";
 
-pub const LIST_MIGRATIONS: &str = "select name from _pyre_migrations;";
+pub const LIST_MIGRATIONS: &str = "select name from _pyre_migrations";
 
 //
 //
 // Creating tables
 
-pub const CREATE_MIGRATION_TABLE: &str = "create table if not exists \"_pyre_migrations\" (
+pub const CREATE_MIGRATION_TABLE: &str = "create table if not exists _pyre_migrations (
     id integer not null primary key autoincrement,
-    createdAt integer not null default (unixepoch()),
-    finishedAt integer,
+    created_at integer not null default (unixepoch()),
+    name text not null,
+    finished_at integer,
     error text,
-    sql text not null,
-    schema_diff text not null
-);";
+    sql text not null
+)";
 
 pub const CREATE_SCHEMA_TABLE: &str = "create table if not exists _pyre_schema (
     id integer not null primary key autoincrement,
-    createdAt integer not null default (unixepoch()),
+    created_at integer not null default (unixepoch()),
     schema text not null
-);";
+)";
 
-pub const INSERT_MIGRATION: &str =
-    "insert into _pyre_migrations (name, sql, schema_diff) values (?, ?, ?);";
+pub const INSERT_MIGRATION_ERROR: &str =
+    "insert into _pyre_migrations (name, sql, error) values (?, ?, ?)";
+
+pub const INSERT_MIGRATION_SUCCESS: &str =
+    "insert into _pyre_migrations (name, sql, finished_at) values (?, ?, unixepoch())";
+
+pub const INSERT_SCHEMA: &str = "insert into _pyre_schema (schema) values (?)";
