8b1cbd7ee2f97b3cc1571d85003a0d2b0f58d7e1
Matthew Griffith <mdg.griffith@gmail.com>
2025-04-21 07:56:53 -0400

    Return SQL in a format that can be used with libsql easily

--- wasm/src/migrate.rs
+++ wasm/src/migrate.rs
@@ -3,7 +3,6 @@ use log::info;
 use pyre::ast;
 use pyre::ast::diff;
 use pyre::db::introspect;
-use pyre::db::migrate;
 use pyre::error;
 use pyre::parser;
 use pyre::typecheck;
@@ -11,8 +10,37 @@ use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen;
 use std::sync::Arc;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 const FILEPATH: &str = "schema.pyre";
+
+/* How migrations should work:
+
+1. Introspect the database
+
+- Database is completely empty?
+  - Run the desired migration which will auto-create
+    the migration table and schema table.
+
+- Database has no migration table or schema table
+
+- Database failed to parse
+
+
+- Parse and typecheck the new schema.
+
+
+
+
+
+*/
+
+#[derive(Serialize)]
+pub struct MigrationSql {
+    pub sql: Vec<SqlAndParams>,
+    pub mark_success: SqlAndParams,
+    pub mark_failure: SqlAndParams,
+}
 
 /**
  * This is a dynamic migration approach.
@@ -21,24 +49,28 @@ const FILEPATH: &str = "schema.pyre";
  *
  */
 pub async fn migrate(
+    name: String,
     introspection: &introspect::Introspection,
     new_schema_source: &str,
-) -> Result<String, Vec<error::Error>> {
+) -> Result<MigrationSql, Vec<error::Error>> {
     // First, parse and typecheck the new schema
 
     // Parse the schema source into a Schema
     let mut new_schema = ast::Schema::default();
     let parse_result = parser::run("schema.pyre", new_schema_source, &mut new_schema);
     if let Err(e) = parse_result {
-        return Err(vec![error::Error {
-            error_type: error::ErrorType::ParsingError(error::ParsingErrorDetails {
-                expecting: error::Expecting::PyreFile,
-            }),
-            filepath: FILEPATH.to_string(),
-            locations: vec![],
-        }]);
+        return match parser::convert_parsing_error(e) {
+            Some(error) => Err(vec![error]),
+            None => Err(vec![error::Error {
+                error_type: error::ErrorType::ParsingError(error::ParsingErrorDetails {
+                    expecting: error::Expecting::PyreFile,
+                }),
+                filepath: FILEPATH.to_string(),
+                locations: vec![],
+            }]),
+        };
     }
-    let schema_clone = new_schema.clone();
+    let new_schema_clone = new_schema.clone();
     // Create a Database from the parsed Schema
     let new_database = ast::Database {
         schemas: vec![new_schema],
@@ -48,8 +80,8 @@ pub async fn migrate(
     let new_context = typecheck::check_schema(&new_database)?;
 
     // Get the recorded schema from introspection
-    let (db_recorded_schema, db_recorded_context) = match &introspection.schema {
-        introspect::SchemaResult::FailedToParse { source, errors } => {
+    let (db_recorded_schema, _db_recorded_context) = match &introspection.schema {
+        introspect::SchemaResult::FailedToParse { errors, .. } => {
             return Err(errors.clone());
         }
         introspect::SchemaResult::FailedToTypecheck { errors, .. } => {
@@ -59,7 +91,7 @@ pub async fn migrate(
     };
 
     // Diff the schemas and check for errors
-    let schema_diff = diff::diff_schema(&db_recorded_schema, &schema_clone);
+    let schema_diff = diff::diff_schema(&db_recorded_schema, &new_schema_clone);
 
     let errors = diff::to_errors(schema_diff);
     if !errors.is_empty() {
@@ -67,52 +99,79 @@ pub async fn migrate(
     }
 
     // Generate the SQL from the diff
-    let db_diff = pyre::db::diff::diff(&new_context, &db_recorded_schema, &introspection);
-    let mut sql = pyre::db::diff::to_sql::to_sql(&db_diff);
+    let db_diff = pyre::db::diff::diff(&new_context, &new_schema_clone, &introspection);
+
+    // Log the db_diff to console
+    console::log_1(&serde_wasm_bindgen::to_value(&db_diff).unwrap());
+    let diff_sql = pyre::db::diff::to_sql::to_sql(&db_diff);
+
+    if pyre::db::diff::is_empty(&db_diff) {
+        return Err(vec![]);
+    }
+
+    let mut sql_executed = String::new();
+    let mut sql = Vec::new();
+    for sql_statement in diff_sql {
+        sql.push(SqlAndParams::Sql(sql_statement.clone()));
+        sql_executed.push_str(&sql_statement);
+        sql_executed.push_str(";\n");
+    }
 
     match introspection.migration_state {
         introspect::MigrationState::NoMigrationTable => {
             // Create the migration table
-            sql.push_str("\n");
-            sql.push_str(pyre::db::migrate::CREATE_MIGRATION_TABLE);
+            sql.push(SqlAndParams::Sql(
+                pyre::db::migrate::CREATE_MIGRATION_TABLE.to_string(),
+            ));
 
             // Create the schema table
-            sql.push_str("\n");
-            sql.push_str(pyre::db::migrate::CREATE_SCHEMA_TABLE);
+            sql.push(SqlAndParams::Sql(
+                pyre::db::migrate::CREATE_SCHEMA_TABLE.to_string(),
+            ));
         }
         introspect::MigrationState::MigrationTable { .. } => {}
     }
+    sql.push(SqlAndParams::SqlWithParams {
+        sql: pyre::db::migrate::INSERT_SCHEMA.to_string(),
+        args: vec![new_schema_source.to_string()],
+    });
 
-    // Insert the migration
-    sql.push_str("\n");
-    sql.push_str(pyre::db::migrate::INSERT_MIGRATION);
-
-    Ok(sql)
+    Ok(MigrationSql {
+        sql,
+        mark_success: SqlAndParams::SqlWithParams {
+            sql: pyre::db::migrate::INSERT_MIGRATION_SUCCESS.to_string(),
+            args: vec![name.to_string(), sql_executed.clone()],
+        },
+        mark_failure: SqlAndParams::SqlWithParams {
+            sql: pyre::db::migrate::INSERT_MIGRATION_ERROR.to_string(),
+            // They'll need to add the error message
+            args: vec![name.to_string(), sql_executed.clone()],
+        },
+    })
 }
 
 #[derive(Serialize)]
-struct MigrateError {
-    errors: Vec<error::Error>,
+#[serde(untagged)]
+pub enum SqlAndParams {
+    Sql(String),
+    SqlWithParams { sql: String, args: Vec<String> },
 }
 
-#[wasm_bindgen]
-pub async fn migrate_wasm(schema_source: String) -> String {
+// #[wasm_bindgen]
+pub async fn migrate_wasm(
+    name: String,
+    schema_source: String,
+) -> Result<MigrationSql, Vec<error::Error>> {
     let introspection = match cache::get() {
         Some(introspection) => introspection,
         None => {
-            return serde_json::to_string(&MigrateError {
-                errors: vec![error::Error {
-                    error_type: error::ErrorType::MigrationMissingSchema,
-                    filepath: "".to_string(),
-                    locations: vec![],
-                }],
-            })
-            .unwrap()
+            return Err(vec![error::Error {
+                error_type: error::ErrorType::MigrationMissingSchema,
+                filepath: "".to_string(),
+                locations: vec![],
+            }]);
         }
     };
 
-    match migrate(&introspection, &schema_source).await {
-        Ok(result) => serde_json::to_string(&result).unwrap(),
-        Err(errors) => serde_json::to_string(&errors).unwrap(),
-    }
+    migrate(name, &introspection, &schema_source).await
 }
