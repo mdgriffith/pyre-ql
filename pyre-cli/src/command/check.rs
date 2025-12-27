a903affbec069eca8e978925faa64c70f78269e2
Matthew Griffith <mdg.griffith@gmail.com>
2025-12-27 08:01:48 -0500

    formatting

--- pyre-cli/src/command/check.rs
+++ pyre-cli/src/command/check.rs
@@ -8,7 +8,10 @@ use pyre::parser;
 use pyre::typecheck;
 
 pub fn check(options: &Options, files: Vec<String>, json: bool) -> io::Result<()> {
-    match run_check(crate::filesystem::collect_filepaths(&options.in_dir)?, options.enable_color) {
+    match run_check(
+        crate::filesystem::collect_filepaths(&options.in_dir)?,
+        options.enable_color,
+    ) {
         Ok(errors) => {
             let has_errors = !errors.is_empty();
             if json {
@@ -25,7 +28,8 @@ pub fn check(options: &Options, files: Vec<String>, json: bool) -> io::Result<()
             } else {
                 for file_error in errors {
                     for err in &file_error.errors {
-                        let formatted_error = error::format_error(&file_error.source, err, options.enable_color);
+                        let formatted_error =
+                            error::format_error(&file_error.source, err, options.enable_color);
                         eprintln!("{}", formatted_error);
                     }
                 }
