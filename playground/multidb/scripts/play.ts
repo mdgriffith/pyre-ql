dcdac7f5e971e28c14d8fe28c056ef20d1a11aca
Matthew Griffith <mdg.griffith@gmail.com>
2026-02-06 12:53:17 -0500

    Remove unused playgrounds

--- playground/multidb/scripts/play.ts
+++
@@ -1,8 +0,0 @@
-import { meta as Games } from '../pyre/generated/typescript/core/queries/metadata/games'
-
-
-(async () => {
-    const base_url = "http://localhost:3000/db";
-    console.log(base_url, Games.id);
-
-})();
